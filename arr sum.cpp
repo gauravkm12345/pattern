23f38d3300dfc2de80919db0d68ba518f41741f7
gauravkm12345 <56034907+gauravkm12345@users.noreply.github.com>
2019-10-01 20:22:45 +0530

    Add files via upload

---
+++ arr sum.cpp
@@ -0,0 +1,16 @@
+//g++  5.4.0
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{int a,u=0;
+int arr[a];
+ cin>>a;
+ for(int i=0;i<a;i++)
+ {
+     cin>>arr[i];
+     u=u+arr[i];
+}
+ return 0;
+}
+    
